a24d9ea2d9b2aad4e3e503b8928b0480baa90ab8
fahadammar <fahadammar96@outlook.com>
2022-02-07 09:59:59 +0500

    The first commit of calculator, working calculator

---
+++ src/com/rfa/Main.java
@@ -0,0 +1,33 @@
+package com.rfa;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                          * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                          / (Math.pow(1+monthlyInterest, numberOfPayments));
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.print("Mortgage: " + mortgageFormatted);
+
+    }
+}
